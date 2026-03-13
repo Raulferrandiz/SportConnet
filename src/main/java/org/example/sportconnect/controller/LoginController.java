0ba9ba8448fa1334fa6ebbdd4413f12d239ac0d6
Raúl Ferrandis <raulman2550@gmail.com>
2026-03-13 16:01:21 +0100

    Login controller añadido

--- src/main/java/org/example/sportconnect/controller/LoginController.java
+++ src/main/java/org/example/sportconnect/controller/LoginController.java
@@ -1,4 +1,48 @@
 package org.example.sportconnect.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.example.sportconnect.service.AuthService;
+import org.example.sportconnect.util.SceneManager;
+
 public class LoginController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label mensajeLabel;
+
+    private final AuthService authService = new AuthService();
+
+    @FXML
+    private void handleLogin() {
+        try {
+            String email = emailField.getText();
+            String password = passwordField.getText();
+
+            authService.login(email, password);
+
+            mensajeLabel.setText("");
+
+            SceneManager.show("/org/example/sportconnect/fxml/home.fxml", "SportConnect - Inicio");
+
+        } catch (IllegalArgumentException e) {
+            mensajeLabel.setText(e.getMessage());
+
+        } catch (Exception e) {
+            mensajeLabel.setText("Error al iniciar sesión.");
+            e.printStackTrace();
+        }
+    }
+    
+    @FXML
+    private void goToRegister() {
+        SceneManager.show("/org/example/sportconnect/fxml/register.fxml", "SportConnect - Registro");
+    }
 }
